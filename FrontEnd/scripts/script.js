988ad7a437c6c73f1cf2189cee9166f23dc523f2
ThomasAlbt <thomas.albert.jv@gmail.com>
2026-08-27 16:50:06 +0200

    a lot

---
+++ FrontEnd/scripts/script.js
@@ -0,0 +1,84 @@
+const api = "http://localhost:5678/api/"
+
+const fetchApi = async (url, endpoint) => {
+    try {
+        const response = await fetch(url + endpoint)
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status} / Endpoint = ${endpoint}`);
+        }
+
+        const results = await response.json()
+        return(results)
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+const portfolio = document.getElementById("portfolio");
+
+const displayProject = (projectList) => {
+    const gallery = portfolio.getElementsByClassName("gallery")[0];
+
+    gallery.replaceChildren()
+
+    projectList.forEach(element => {
+        gallery.innerHTML += 
+        `
+            <figure>
+                <img src=${element.imageUrl} alt=${element.title}>
+                <figcaption>${element.title}<figcatpion/>
+            <figure/>
+        `
+    });
+}
+
+const displayCategories = (categoriesList, projectList) => {
+    const filters = portfolio.querySelector(".filters");
+    const template = document.getElementById("category-template");
+
+    filters.replaceChildren();
+
+    const showAll = template.content.firstElementChild.cloneNode(true);
+    showAll.textContent = "Tous";
+    showAll.dataset.categoryId = "0";
+    showAll.classList.add("active");
+    filters.append(showAll);
+
+    categoriesList.forEach(element => {
+        const button = template.content.firstElementChild.cloneNode(true);
+
+        button.textContent = element.name;
+        button.dataset.categoryId = element.id;
+
+        filters.append(button);
+    });
+
+    filterProjects(projectList);
+}
+
+const filterProjects = (projectList) => {
+    const filters = portfolio.querySelector(".filters");
+
+    filters.addEventListener("click", (event) => {
+        const categoryId = Number(event.target.dataset.categoryId);
+
+        const filteredProjects = categoryId === 0
+            ? projectList
+            : projectList.filter((project) => {
+                return project.categoryId === categoryId
+            });
+            
+            displayProject(filteredProjects);
+    })
+}
+
+const main = async () => {
+    const projectList = await fetchApi(api, "works");
+    const categoriesList = await fetchApi(api, "categories");
+
+    displayProject(projectList);
+    displayCategories(categoriesList, projectList);
+}
+
+main()
